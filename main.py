251b680e60f6d7ad7877cb4606372628482b848a
niceric <niclasericsson89@hotmail.com>
2024-04-20 00:04:30 +0200

    added counter for characters/letters and a prettier print out

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    print(get_number_of_letters(text))
-    #print(type(text))
-    print(get_total_number_of_words(text))
-
-    # ...
+    path = "books/frankenstein.txt" 
+    text = get_book_text(path)   
+    print_report(path, text)
+    
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
     
-def get_total_number_of_words(words):
+def get_total_number_of_words(text):
     counter = 0 
-    total_words = words.split()
-    #print(total_words)
+    total_words = text.split()
+ 
     for word in total_words:
         counter += 1
     return counter
@@ -30,16 +27,33 @@ def get_number_of_letters(text):
             letters[lowered] = 1
     return letters 
 
+def get_list_of_words(text):
+        count_of_letters = get_number_of_letters(text)
+        x = []
 
         
+        def create_list(text):
+            for letter in count_of_letters:
+                if letter.isalpha():
+                    temp_dict = dict(char = letter, num = count_of_letters[letter])
+                    x.append(temp_dict)
+                    
         
-        #letters
-        #if letter not in letters:
+            def sort_on(dict):
+                return dict["num"]
+            x.sort(reverse=True, key=sort_on)
+        
+        create_list(count_of_letters)
+        return x        
+        
 
+def print_report(path, text):
+    print(f"__..---- Beginning of report from {path} ----..__")  
+    print(f"Total number of words: {get_total_number_of_words(text)}")
+    get_list_of_words(text)
+    for dict in get_list_of_words(text): 
         
-
+        print(f"The '{dict["char"]}' character was found {dict["num"]} times.")
         
-        #print(letter)
-    
 
 main()
