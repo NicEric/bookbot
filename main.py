18e76566958fd45660319ce4510506564292e642
NicEric <niclasericsson89@hotmail.com>
2024-04-19 11:35:26 +0200

    added function for counting each char/letter

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    #print(text)
-    print(get_number_of_words(text))
+    print(get_number_of_letters(text))
+    #print(type(text))
+    print(get_total_number_of_words(text))
 
     # ...
 
@@ -10,13 +11,35 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
     
-def get_number_of_words(words):
+def get_total_number_of_words(words):
     counter = 0 
     total_words = words.split()
-    print(total_words)
+    #print(total_words)
     for word in total_words:
         counter += 1
     return counter
 
+def get_number_of_letters(text):
+    letters = {}
+
+    for letter in text:
+        lowered = letter.lower()
+        if lowered in letters:
+            letters[lowered] += 1
+        else:
+            letters[lowered] = 1
+    return letters 
+
+
+        
+        
+        #letters
+        #if letter not in letters:
+
+        
+
+        
+        #print(letter)
+    
 
 main()
